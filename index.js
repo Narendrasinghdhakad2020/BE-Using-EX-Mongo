1d5ae4c49e7f4da468f145cceb6df09d13885f4e
Narendra singh Dhakad <narendrasinghdhakad2020@gmail.com>
2025-01-31 11:42:14 +0530

    modifying index.js file

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/resource", resourceRoutes);
 
 // Server Listening
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
